ada857afc8e49a9c47584c705f4afa03bf249b11
Bartlomiej Guminiak <guminiak.bartlomiej@gmail.com>
2019-01-30 16:06:53 +0100

    Add initial iOS project structure

---
+++ MeditationAppShowcase/MeditationAppShowcase/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MeditationAppShowcase
+//
+//  Created by Bartlomiej Guminiak on 30/01/2019.
+//  Copyright © 2019 Bartlomiej Guminiak. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+
+}
+
---
+++ MeditationAppShowcase/MeditationAppShowcase/ViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
