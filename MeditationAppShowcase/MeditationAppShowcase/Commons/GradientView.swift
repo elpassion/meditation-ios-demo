4fed03e2746b9f37b0b8801a1bc759ef27e18bbb
Bartlomiej <guminiak.bartlomiej@gmail.com>
2019-02-07 17:13:50 +0100

    Add GradientView

---
+++ MeditationAppShowcase/MeditationAppShowcase/Commons/GradientView.swift
@@ -0,0 +1,52 @@
+import UIKit
+
+class GradientView: UIView {
+
+    init(style: GradientStyle) {
+        super.init(frame: .zero)
+        isUserInteractionEnabled = false
+        gradientLayer.colors = style.colors.map { $0.color.cgColor }
+        gradientLayer.locations = style.colors.map { NSNumber(value: $0.location) }
+        gradientLayer.startPoint = style.direction.startPoint
+        gradientLayer.endPoint = style.direction.endPoint
+        layer.addSublayer(gradientLayer)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        return nil
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = layer.bounds
+    }
+
+    private let gradientLayer = CAGradientLayer()
+
+}
+
+struct GradientStyle {
+    let colors: [GradientColor]
+    let direction: GradientDirection
+}
+
+struct GradientColor {
+    let location: Float
+    let color: UIColor
+}
+
+struct GradientDirection {
+    let startPoint: CGPoint
+    let endPoint: CGPoint
+}
+
+extension GradientDirection {
+
+    static var vertical: GradientDirection {
+        return GradientDirection(
+            startPoint: CGPoint(x: 0.5, y: 0),
+            endPoint: CGPoint(x: 0.5, y: 1)
+        )
+    }
+
+}
