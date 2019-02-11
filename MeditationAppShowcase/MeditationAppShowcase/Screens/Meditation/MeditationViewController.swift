4ddac816c7ea64ab35671da7200071386bca5bee
Bartlomiej Guminiak <guminiak.bartlomiej@gmail.com>
2019-02-11 20:06:40 +0100

    Add empty MeditationViewController

---
+++ MeditationAppShowcase/MeditationAppShowcase/Screens/Meditation/MeditationViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class MeditationViewController: UIViewController {
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) { return nil }
+
+}
