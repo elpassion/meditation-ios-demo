3781dc4d28fb2bc4b9e02ae234d13f18b7926f3d
Bartlomiej Guminiak <guminiak.bartlomiej@gmail.com>
2019-02-12 21:05:47 +0100

    Add empty SongPickerViewCell

---
+++ MeditationAppShowcase/MeditationAppShowcase/Screens/Meditation/SongPickerViewCell.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class SongPickerViewCell: UITableViewCell {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+
+    required init?(coder aDecoder: NSCoder) { return nil }
+
+}
