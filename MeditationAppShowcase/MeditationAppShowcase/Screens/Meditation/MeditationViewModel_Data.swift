c9635714dd7107da1f79886981e66b323045e794
Bartlomiej Guminiak <guminiak.bartlomiej@gmail.com>
2019-02-13 14:34:31 +0100

    Add MeditationViewModel_Data

---
+++ MeditationAppShowcase/MeditationAppShowcase/Screens/Meditation/MeditationViewModel_Data.swift
@@ -0,0 +1,48 @@
+extension MeditationViewModel {
+
+    var preparedViewModels: [SongPickerViewModeling] {
+        return [
+            SongPickerViewModel(appearanceAnimationDelay: 0,
+                                title: "Walking with the wolves",
+                                subtitle: "KArl Roys",
+                                time: "2:50"),
+            SongPickerViewModel(appearanceAnimationDelay: 0.1,
+                                title: "Meditation Intro",
+                                subtitle: "Piter Marqo",
+                                time: "5:30"),
+            SongPickerViewModel(appearanceAnimationDelay: 0.2,
+                                title: "Basic Meditation Song",
+                                subtitle: "Menero Riger",
+                                time: "5:30"),
+            SongPickerViewModel(appearanceAnimationDelay: 0.3,
+                                title: "Meditation Level 1",
+                                subtitle: "Agnes Inex",
+                                time: "1:30"),
+            SongPickerViewModel(appearanceAnimationDelay: 0.4,
+                                title: "Advanced Meditation Song",
+                                subtitle: "Casaba Martin",
+                                time: "1:11"),
+            SongPickerViewModel(appearanceAnimationDelay: 0.5,
+                                title: "Relaxing Music",
+                                subtitle: "Casaba Martin",
+                                time: "2:45"),
+            SongPickerViewModel(appearanceAnimationDelay: 0.6,
+                                title: "Relaxing Sea",
+                                subtitle: "Bob Marley",
+                                time: "7:04"),
+            SongPickerViewModel(appearanceAnimationDelay: 0.7,
+                                title: "Meditation Level 2",
+                                subtitle: "Aston Martin",
+                                time: "3:44"),
+            SongPickerViewModel(appearanceAnimationDelay: 0.8,
+                                title: "Just Breath Deeply",
+                                subtitle: "Mentero Riger",
+                                time: "7:13"),
+            SongPickerViewModel(appearanceAnimationDelay: 0.9,
+                                title: "Sunrise and Sunset",
+                                subtitle: "Agnex Inez",
+                                time: "3:56")
+        ]
+    }
+
+}
