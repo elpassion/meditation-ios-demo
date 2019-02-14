74384e19bdb853135e97f1f4e11e4efe7158008e
Bartlomiej Guminiak <guminiak.bartlomiej@gmail.com>
2019-02-14 16:06:53 +0100

    Add ActionView

---
+++ MeditationAppShowcase/MeditationAppShowcase/Commons/ActionViewController/ActionView.swift
@@ -0,0 +1,69 @@
+import UIKit
+
+class ActionView: UIView {
+
+    init() {
+        super.init(frame: .zero)
+        backgroundColor = .red
+        addSubviews()
+        setupLayout()
+    }
+
+    required init?(coder aDecoder: NSCoder) { return nil }
+
+    let rewindButton = SubviewFactory.rewindButton()
+    let middleButton = SubviewFactory.middleButton()
+    let forwardButton = SubviewFactory.forwardButton()
+
+    // MARK: - privates
+
+    private func addSubviews() {
+        addSubview(rewindButton)
+        addSubview(middleButton)
+        addSubview(forwardButton)
+    }
+
+    private func setupLayout() {
+        let distance: CGFloat = 40
+        NSLayoutConstraint.activate([
+            rewindButton.centerYAnchor.constraint(equalTo: middleButton.centerYAnchor),
+            rewindButton.trailingAnchor.constraint(equalTo: middleButton.leadingAnchor,
+                                                   constant: -distance),
+            middleButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+            middleButton.topAnchor.constraint(equalTo: topAnchor),
+            middleButton.bottomAnchor.constraint(equalTo: bottomAnchor),
+            forwardButton.leadingAnchor.constraint(equalTo: middleButton.trailingAnchor,
+                                                   constant: distance)
+        ])
+    }
+
+}
+
+private extension ActionView {
+
+    struct SubviewFactory {
+
+        static func rewindButton() -> UIButton {
+            let button = UIButton(frame: .zero)
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.setTitle("<<", for: .normal)
+            return button
+        }
+
+        static func middleButton() -> ActionButton {
+            let button = ActionButton()
+            button.setTitle("sada dasf ", for: .normal) //TODO:
+            button.translatesAutoresizingMaskIntoConstraints = false
+            return button
+        }
+
+        static func forwardButton() -> UIButton {
+            let button = UIButton(frame: .zero)
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.setTitle(">>", for: .normal)
+            return button
+        }
+
+    }
+
+}
