563c29593cde2e3208e80c32e3b7292a98a87091
Bartlomiej Guminiak <guminiak.bartlomiej@gmail.com>
2019-02-18 20:42:39 +0100

    Add convenience methods for building constraints

--- MeditationAppShowcase/MeditationAppShowcase/Commons/UIView_Layout.swift
+++ MeditationAppShowcase/MeditationAppShowcase/Commons/UIView_Layout.swift
@@ -14,10 +14,14 @@ extension UIView {
                                 constant: -edges.bottom).isActive = true
     }
 
+    // MARK: - Size
+
     func pinHeight(_ height: CGFloat) {
         translatesAutoresizingMaskIntoConstraints = false
         heightAnchor.constraint(equalToConstant: height).isActive = true
     }
+
+    // MARK: - Sides
 
     func pinTop(to yAxis: NSLayoutAnchor<NSLayoutYAxisAnchor>, offset: CGFloat = 0) {
         translatesAutoresizingMaskIntoConstraints = false
@@ -37,6 +41,18 @@ extension UIView {
     func pinBottom(to yAxis: NSLayoutAnchor<NSLayoutYAxisAnchor>, offset: CGFloat = 0) {
         translatesAutoresizingMaskIntoConstraints = false
         bottomAnchor.constraint(equalTo: yAxis, constant: offset).isActive = true
+    }
+
+    // MARK: - Centers
+
+    func pinCenterX(to xAxis: NSLayoutAnchor<NSLayoutXAxisAnchor>, offset: CGFloat = 0) {
+        translatesAutoresizingMaskIntoConstraints = false
+        centerXAnchor.constraint(equalTo: xAxis, constant: offset).isActive = true
+    }
+
+    func pinCenterY(to yAxis: NSLayoutAnchor<NSLayoutYAxisAnchor>, offset: CGFloat = 0) {
+        translatesAutoresizingMaskIntoConstraints = false
+        centerYAnchor.constraint(equalTo: yAxis, constant: offset).isActive = true
     }
 
 }
