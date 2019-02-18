07addd58868923c92c66110c3085b76f953817ac
Bartlomiej Guminiak <guminiak.bartlomiej@gmail.com>
2019-02-18 09:59:42 +0100

    Add ActionViewModel

---
+++ MeditationAppShowcase/MeditationAppShowcase/Screens/ActionController/ActionViewModel.swift
@@ -0,0 +1,55 @@
+import Foundation
+
+protocol ActionViewModeling: class {
+    func rewindButtonAction()
+    func middleButtonAction()
+    func forwardButtonAction()
+    var currentMode: ((ActionViewController.Mode) -> Void)? { get set }
+}
+
+protocol ActionOperating {
+    var actionHandler: ActionHandling { get }
+    func set(mode: ActionViewController.Mode)
+}
+
+class ActionViewModel: ActionViewModeling, ActionOperating {
+
+    init(actionEmitter: ActionHandling & ActionEmitter = ActionEmitter()) {
+        self.actionEmitter = actionEmitter
+    }
+
+    // MARK: - ActionViewModeling
+
+    func rewindButtonAction() {
+        actionEmitter.emit(.rewind)
+    }
+
+    func middleButtonAction() {
+        switch mode {
+        case .singleButton: actionEmitter.emit(.button)
+        case .player: actionEmitter.emit(.play)
+        }
+    }
+
+    func forwardButtonAction() {
+        actionEmitter.emit(.forward)
+    }
+
+    var currentMode: ((ActionViewController.Mode) -> Void)?
+
+    // MARK: - ActionOperating
+
+    var actionHandler: ActionHandling {
+        return actionEmitter
+    }
+
+    func set(mode: ActionViewController.Mode) {
+        currentMode?(mode)
+    }
+
+    // MARK: - Privates
+
+    private let actionEmitter: ActionHandling & ActionEmitter
+    private var mode: ActionViewController.Mode = .singleButton(title: "")
+
+}
