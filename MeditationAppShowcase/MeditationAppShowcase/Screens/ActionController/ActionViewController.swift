a630a58ece2fb67989f10ccc08d47f7eae5c7ab8
Bartlomiej Guminiak <guminiak.bartlomiej@gmail.com>
2019-02-18 12:03:19 +0100

    ActionViewController cleanups

--- MeditationAppShowcase/MeditationAppShowcase/Screens/ActionController/ActionViewController.swift
+++ MeditationAppShowcase/MeditationAppShowcase/Screens/ActionController/ActionViewController.swift
@@ -1,15 +1,11 @@
 import UIKit
 
-protocol ActionViewControlling: class {
-    var currentMode: ActionViewController.Mode { get set }
-    var actionHandler: ActionHandling { get }
-//    var actionHandler: EventHandling<ActionViewController.Action> { get }
-}
+class ActionViewController: UIViewController {
 
-class ActionViewController: UIViewController, ActionViewControlling {
-
-    init(actionEmitter: ActionHandling & ActionEmitter = ActionEmitter()) {
-        self.actionEmitter = actionEmitter
+    init(viewModel: ActionViewModeling,
+         animator: Animating) {
+        self.viewModel = viewModel
+        self.animator = animator
         super.init(nibName: nil, bundle: nil)
     }
 
@@ -21,43 +17,22 @@ class ActionViewController: UIViewController, ActionViewControlling {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureInitialState()
-        configureButtons()
-    }
-
-    var currentMode: Mode = .singleButton(title: "") {
-        didSet {
-            switch (oldValue, currentMode) {
-            case (.singleButton, .singleButton(let title)):
-                singleToSingle(title: title)
-            case (.singleButton, .player):
-                singleButtonToPlayer()
-            case (.player, .singleButton(let title)):
-                playerToSingleButton(title: title)
-            case (.player, .player): ()
-            }
-        }
-    }
-
-    var actionHandler: ActionHandling {
-        return actionEmitter
+        configure()
     }
 
     // MARK: - Privates
 
-    private let actionEmitter: ActionHandling & ActionEmitter //TODO: move to viewModel
+    private let viewModel: ActionViewModeling
+    private let animator: Animating
     private let animationDuration: TimeInterval = 0.6
 
     private var actionView: ActionView! {
         return view as? ActionView
     }
 
-    private func configureInitialState() {
+    private func configure() {
         actionView.rewindButton.alpha = 0
         actionView.forwardButton.alpha = 0
-    }
-
-    private func configureButtons() {
         actionView.rewindButton.addTarget(self,
                                           action: #selector(rewindButtonAction),
                                           for: .touchUpInside)
@@ -67,11 +42,40 @@ class ActionViewController: UIViewController, ActionViewControlling {
         actionView.forwardButton.addTarget(self,
                                           action: #selector(forwardButtonAction),
                                           for: .touchUpInside)
+        viewModel.currentMode = { [weak self] in self?.mode = $0 }
     }
 
-    // MARK: - Transformations
+    // MARK: - Button action handlers
 
-    private func singleToSingle(title: String) {
+    @objc private func rewindButtonAction() {
+        viewModel.rewindButtonAction()
+    }
+
+    @objc private func middleButtonAction() {
+        viewModel.middleButtonAction()
+    }
+
+    @objc private func forwardButtonAction() {
+        viewModel.forwardButtonAction()
+    }
+
+    // MARK: - Mode transformations
+
+    private var mode: Mode = .singleButton(title: "") {
+        didSet {
+            switch (oldValue, mode) {
+            case (.singleButton, .singleButton(let title)):
+                singleButtonToSingleButton(title: title)
+            case (.singleButton, .player):
+                singleButtonToPlayer()
+            case (.player, .singleButton(let title)):
+                playerToSingleButton(title: title)
+            case (.player, .player): ()
+            }
+        }
+    }
+
+    private func singleButtonToSingleButton(title: String) {
         animate(animations: {
             self.actionView.set(mode: .singleButton(title: title))
             self.actionView.middleControl.layoutIfNeeded()
@@ -79,7 +83,6 @@ class ActionViewController: UIViewController, ActionViewControlling {
     }
 
     private func singleButtonToPlayer() {
-
         animate(animations: {
             self.actionView.set(mode: .player)
             self.actionView.middleControl.layoutIfNeeded()
@@ -119,30 +122,10 @@ class ActionViewController: UIViewController, ActionViewControlling {
 
     private func animate(animations: @escaping () -> Void,
                          delay: TimeInterval = 0) {
-        UIView.animate(withDuration: animationDuration,
-                       delay: delay,
-                       usingSpringWithDamping: 0.6,
-                       initialSpringVelocity: 0.5,
-                       options: [.curveEaseInOut, .allowUserInteraction],
-                       animations: animations,
-                       completion: nil)
-    }
-
-    // MARK: - Action Handlers
-
-    @objc private func rewindButtonAction() {
-        actionEmitter.emit(.rewind)
-    }
-
-    @objc private func forwardButtonAction() {
-        actionEmitter.emit(.forward)
-    }
-
-    @objc private func middleButtonAction() {
-        switch currentMode {
-        case .singleButton: actionEmitter.emit(.button)
-        case .player: actionEmitter.emit(.play)
-        }
+        animator.animate(duration: animationDuration,
+                         delay: delay,
+                         options: [.curveEaseInOut, .allowUserInteraction],
+                         animations: animations)
     }
 
 }
