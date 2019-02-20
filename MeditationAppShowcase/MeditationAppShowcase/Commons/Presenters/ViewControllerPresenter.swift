86cb4f9cb846af63857cbb21d6fb89a5b1ea128b
Bartlomiej Guminiak <guminiak.bartlomiej@gmail.com>
2019-02-20 22:19:34 +0100

    Improve viewController presenter

--- MeditationAppShowcase/MeditationAppShowcase/Commons/Presenters/ViewControllerPresenter.swift
+++ MeditationAppShowcase/MeditationAppShowcase/Commons/Presenters/ViewControllerPresenter.swift
@@ -10,11 +10,24 @@ class ViewControllerPresenter: ViewControllerPresenting {
     // MARK: - ViewControllerPresenting
 
     func present(viewController: UIViewController, on base: UIViewController) {
-        base.present(viewController, animated: false, completion: nil)
+        viewController.willMove(toParent: base)
+        base.beginAppearanceTransition(false, animated: false)
+        viewController.beginAppearanceTransition(true, animated: false)
+        base.addChild(viewController)
+        base.view.addSubview(viewController.view)
+        viewController.view.pinEdges(to: base.view)
+        viewController.didMove(toParent: base)
+        viewController.endAppearanceTransition()
     }
 
     func dismiss(viewController: UIViewController) {
-        viewController.dismiss(animated: false, completion: nil)
+        viewController.beginAppearanceTransition(false, animated: false)
+        viewController.parent?.beginAppearanceTransition(true, animated: false)
+        viewController.willMove(toParent: viewController.parent)
+        viewController.view.removeFromSuperview()
+        viewController.didMove(toParent: viewController.parent)
+        viewController.parent?.endAppearanceTransition()
+        viewController.removeFromParent()
     }
 
 }
