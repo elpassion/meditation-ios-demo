dff16ee8e96b9c4fabf693c5e99fea9b622c0008
Bartlomiej Guminiak <guminiak.bartlomiej@gmail.com>
2019-02-21 13:47:43 +0100

    Refactor ViewControllerPresenter

--- MeditationAppShowcase/MeditationAppShowcase/Commons/Presenters/ViewControllerPresenter.swift
+++ MeditationAppShowcase/MeditationAppShowcase/Commons/Presenters/ViewControllerPresenter.swift
@@ -10,24 +10,11 @@ class ViewControllerPresenter: ViewControllerPresenting {
     // MARK: - ViewControllerPresenting
 
     func present(viewController: UIViewController, on base: UIViewController) {
-        viewController.willMove(toParent: base)
-        base.beginAppearanceTransition(false, animated: false)
-        viewController.beginAppearanceTransition(true, animated: false)
-        base.addChild(viewController)
-        base.view.addSubview(viewController.view)
-        viewController.view.pinEdges(to: base.view)
-        viewController.didMove(toParent: base)
-        viewController.endAppearanceTransition()
+        base.embed(viewController: viewController, in: base.view)
     }
 
     func dismiss(viewController: UIViewController) {
-        viewController.beginAppearanceTransition(false, animated: false)
-        viewController.parent?.beginAppearanceTransition(true, animated: false)
-        viewController.willMove(toParent: viewController.parent)
-        viewController.view.removeFromSuperview()
-        viewController.didMove(toParent: viewController.parent)
-        viewController.parent?.endAppearanceTransition()
-        viewController.removeFromParent()
+        viewController.unembed()
     }
 
 }
