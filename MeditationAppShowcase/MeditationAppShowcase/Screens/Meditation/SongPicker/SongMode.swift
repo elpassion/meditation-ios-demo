484216a7245e4b43119842eba09d4800971fb4f4
Bartlomiej Guminiak <guminiak.bartlomiej@gmail.com>
2019-02-22 12:16:25 +0100

    Update SongMode

--- MeditationAppShowcase/MeditationAppShowcase/Screens/Meditation/SongPicker/SongMode.swift
+++ MeditationAppShowcase/MeditationAppShowcase/Screens/Meditation/SongPicker/SongMode.swift
@@ -1,3 +1,12 @@
+enum Picking {
+    case selected, unselected
+}
+
+enum Listening {
+    case playable, playing, hidden
+}
+
 enum SongMode {
-    case selectable, selected, playable, playing, hidden
+    case picking(Picking)
+    case listening(Listening)
 }
