d1b3046f35e5577e1781dd12c3950159f062cc3d
Bartlomiej Guminiak <guminiak.bartlomiej@gmail.com>
2019-02-25 22:22:44 +0100

    Add SongInteractiveView_Height

---
+++ MeditationAppShowcase/MeditationAppShowcase/Screens/Meditation/SongPicker/SongInteractive/SongInteractionView_Height.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+extension SongInteractiveView {
+
+    static var playableHeight: CGFloat {
+        return 76
+    }
+
+    static var playingHeight: CGFloat {
+        return 125
+    }
+
+}
